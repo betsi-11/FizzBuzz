74dc89e74f3c0f142560166765d18a5cb10680af
Betselot Tasew <92887357+betsi-11@users.noreply.github.com>
2021-10-20 23:34:51 +0300

    Update output.php

--- output.php
+++ output.php
@@ -1,19 +1,19 @@
 for ($i=0; $i<=100; $i++){
     if($i % 3==0 && $i % 5==0){
-        echo "FizzBuzz \n";
+        echo "FizzBuzz <br>";
         
     }
     else if($i % 3==0){
-        echo "Fizz \n";
+        echo "Fizz <br>";
         
     }
     else if($i % 5==0){
-        echo "Buzz \n";
+        echo "Buzz <br>";
         
     }
     else {
         echo $i;
-        echo "\n";
+        echo "<br>";
         
     }
 
